09053a4561d6c1222764f419c23ec72d94171268
Dixxxneufff <sergio.dixneuf@estudiantes.uva.es>
2024-12-11 19:50:43 +0100

    'Cuestionario1'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+	title: "Proyecto Hotel Paraíso", 
+    description: "Explora la página oficial de nuestro Hotel Paraíso y descubre todo lo que tiene para ofrecer.",
+    renderLink: "https://lab-1-ctmj.onrender.com"
+  });
 });
 
 module.exports = router;
